50f8e0b1869b9451019c65a09024b1a8d8177027
Darian Brown <Darian Brown>
2025-02-10 18:36:52 -0400

    Create AboutController and About page with layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Darian Brown</h2>
+    <p>I am a passionate software engineering student working on improving my web development skills. I love coding, learning new technologies and building creative solutions.</p>
+@endsection
