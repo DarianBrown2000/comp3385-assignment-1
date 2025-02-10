01ce8e13172fac4a3d534c058e661c275b9813e3
Darian Brown <Darian Brown>
2025-02-10 19:03:06 -0400

    Styled the page and added an image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     <h2>Darian Brown</h2>
     <p>I am a passionate software engineering student working on improving my web development skills. I love coding, learning new technologies and building creative solutions.</p>
-@endsection
+    <img src="{{ url('/images/profile.jpg') }}" alt="My Profile Picture" />
+    @endsection
